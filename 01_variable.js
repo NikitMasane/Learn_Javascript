e05e70c4daed037f89c5af6eb521ca3fa4a62949
prathmeshpahune <prathmeshpahune24@gmail.com>
2024-01-25 15:47:36 +0530

    learn variable

---
+++ 01_variable.js
@@ -0,0 +1,14 @@
+const account= 144553
+let Email = "nikitamasane@gmail.com"
+var password  = "12345"
+city ="wardha"
+let AccountDetails; //undefined
+
+//account = 2 // not allowed
+
+ Email = "nikita@gmail.com"
+ password  = "5678"
+city ="nagpur"
+
+console.log(account)
+console.table([account,Email,password,city,AccountDetails])
